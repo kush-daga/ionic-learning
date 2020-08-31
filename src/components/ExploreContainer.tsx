aeb24409a3b846aa06f7a8a0137a9bcba3205967
Kush Daga <kushdaga@hotmail.com>
2020-08-31 23:14:20 +0530

    added list

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,14 +1,65 @@
-import React from 'react';
-import './ExploreContainer.css';
+import React from "react";
+import {
+  IonContent,
+  IonList,
+  IonItem,
+  IonText,
+  IonAvatar,
+  IonLabel,
+  IonItemOption,
+  IonItemOptions,
+  IonItemSliding,
+} from "@ionic/react";
 
-interface ContainerProps { }
+interface ContainerProps {}
 
+const listRaw = [
+  {
+    name: "Kush",
+    desc: "Kush is a good boy",
+  },
+  {
+    name: "Jay",
+    desc: "jay is also a good boy",
+  },
+  {
+    name: "Hemant",
+    desc: "He is an ok boy, but cute",
+  },
+  {
+    name: "Jaskee",
+    desc: "he is also good :))",
+  },
+];
 const ExploreContainer: React.FC<ContainerProps> = () => {
   return (
-    <div className="container">
-      <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-    </div>
+    <>
+      <IonList>
+        {listRaw.map((elem) => {
+          return (
+            <IonItemSliding key={elem.name}>
+              <IonItem>
+                <IonAvatar>
+                  <img
+                    src={`https://source.unsplash.com/random/200x200`}
+                    alt="New"
+                  />
+                </IonAvatar>
+                <IonLabel className="ion-padding">
+                  <h2>{elem.name}</h2>
+                  <h3>{elem.desc}</h3>
+                </IonLabel>
+              </IonItem>
+              <IonItemOptions side="end">
+                <IonItemOption onClick={() => alert("hello")}>
+                  Hello
+                </IonItemOption>
+              </IonItemOptions>
+            </IonItemSliding>
+          );
+        })}
+      </IonList>
+    </>
   );
 };
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,26 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import React from "react";
+import ExploreContainer from "../components/ExploreContainer";
+import "./Home.css";
 
 const Home: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>CORE X</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle size="large">CORE X</IonTitle>
           </IonToolbar>
         </IonHeader>
         <ExploreContainer />
